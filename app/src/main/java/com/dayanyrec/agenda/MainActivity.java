77a98b61e03a12cf5fe1eb80639b9792ecd4ad91
Dayany Espindola <dayanyrec@gmail.com>
2021-06-01 23:53:20 -0300

    Use layout resource instead of TextView

--- app/src/main/java/com/dayanyrec/agenda/MainActivity.java
+++ app/src/main/java/com/dayanyrec/agenda/MainActivity.java
@@ -2,7 +2,6 @@ package com.dayanyrec.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -10,8 +9,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Dayany Espindola");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
     }
 }

702277688a148a155238e465d5786b868172cb2f
Dayany Espindola <dayanyrec@gmail.com>
2021-06-02 00:07:20 -0300

    Add a list of names

--- app/src/main/java/com/dayanyrec/agenda/MainActivity.java
+++ app/src/main/java/com/dayanyrec/agenda/MainActivity.java
@@ -2,13 +2,25 @@ package com.dayanyrec.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Day"));
+        TextView primeiroAluno = findViewById(R.id.textView);
+        primeiroAluno.setText(alunos.get(0));
+        TextView segundoAluno = findViewById(R.id.textView2);
+        segundoAluno.setText(alunos.get(1));
+        TextView terceiroAluno = findViewById(R.id.textView3);
+        terceiroAluno.setText(alunos.get(2));
     }
 }

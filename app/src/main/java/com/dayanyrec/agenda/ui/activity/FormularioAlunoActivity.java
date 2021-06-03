82140f13ebcd7a37ec64c5ec376205fbad9d8550
Dayany Espindola <dayanyrec@gmail.com>
2021-06-03 18:47:01 -0300

    Create new activity formulario de alunos and organize ui package

---
+++ app/src/main/java/com/dayanyrec/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package com.dayanyrec.agenda.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dayanyrec.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
--- app/src/main/java/com/dayanyrec/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/dayanyrec/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,4 +1,4 @@
-package com.dayanyrec.agenda;
+package com.dayanyrec.agenda.ui.activity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -8,18 +8,20 @@ import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.dayanyrec.agenda.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Day"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
         listaDeAlunos.setAdapter(adapter);
     }

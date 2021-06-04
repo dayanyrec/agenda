084fb6164dedc9166929116323a12c9da8c23585
Dayany Espindola <dayanyrec@gmail.com>
2021-06-04 17:03:04 -0300

    Add save action in the form

---
+++ app/src/main/java/com/dayanyrec/agenda/model/Aluno.java
@@ -0,0 +1,34 @@
+package com.dayanyrec.agenda.model;
+
+public class Aluno {
+    private final String nome;
+    private final String telefone;
+    private final String email;
+
+    public Aluno(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/com/dayanyrec/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/dayanyrec/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,10 +1,15 @@
 package com.dayanyrec.agenda.ui.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dayanyrec.agenda.R;
+import com.dayanyrec.agenda.model.Aluno;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +17,18 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final EditText nome = findViewById(R.id.activity_formulario_nome);
+        final EditText telefone = findViewById(R.id.activity_formulario_telefone);
+        final EditText email = findViewById(R.id.activity_formulario_email);
+
+        findViewById(R.id.activity_formulario_botao_salvar)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Aluno aluno = new Aluno(nome.getText().toString(), telefone.getText().toString(), email.getText().toString());
+                        Toast.makeText(FormularioAlunoActivity.this, aluno.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }

e88db4f208ddf7007d9832ad616fb5ae864de32a
Dayany Espindola <dayanyrec@gmail.com>
2021-06-04 21:04:58 -0300

    Refactor

--- app/src/main/java/com/dayanyrec/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/dayanyrec/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,40 +1,57 @@
 package com.dayanyrec.agenda.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dayanyrec.agenda.R;
 import com.dayanyrec.agenda.dao.AlunoDao;
 import com.dayanyrec.agenda.model.Aluno;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
+
+    public static final String NOVO_ALUNO = "Novo aluno";
+    private EditText nome;
+    private EditText telefone;
+    private EditText email;
+    private final AlunoDao alunoDao = new AlunoDao();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+        setTitle(NOVO_ALUNO);
+        inicializaCampos();
+        configuraBotaoSalvar();
+    }
 
-        final EditText nome = findViewById(R.id.activity_formulario_nome);
-        final EditText telefone = findViewById(R.id.activity_formulario_telefone);
-        final EditText email = findViewById(R.id.activity_formulario_email);
+    private void inicializaCampos() {
+        nome = findViewById(R.id.activity_formulario_nome);
+        telefone = findViewById(R.id.activity_formulario_telefone);
+        email = findViewById(R.id.activity_formulario_email);
+    }
 
-        final AlunoDao alunoDao = new AlunoDao();
-
+    private void configuraBotaoSalvar() {
         findViewById(R.id.activity_formulario_botao_salvar)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Aluno aluno = new Aluno(nome.getText().toString(), telefone.getText().toString(), email.getText().toString());
-
+                        Aluno aluno = criaAluno();
                         alunoDao.salva(aluno);
                         finish();
                     }
                 });
+    }
+
+    @NonNull
+    private Aluno criaAluno() {
+        String nome = this.nome.getText().toString();
+        String telefone = this.telefone.getText().toString();
+        String email = this.email.getText().toString();
+
+        return new Aluno(nome, telefone, email);
     }
 }
--- app/src/main/java/com/dayanyrec/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/dayanyrec/agenda/ui/activity/ListaAlunosActivity.java
@@ -13,31 +13,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.dayanyrec.agenda.R;
 import com.dayanyrec.agenda.dao.AlunoDao;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class ListaAlunosActivity extends AppCompatActivity {
+    public static final String LISTA_DE_ALUNOS = "Lista de Alunos";
+    private final AlunoDao alunoDao = new AlunoDao();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-        setTitle("Lista de Alunos");
+        setTitle(LISTA_DE_ALUNOS);
+        configuraFabNovoAluno();
+    }
 
+    private void configuraFabNovoAluno() {
         findViewById(R.id.activity_lista_alunos_fab_novo_aluno).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class));
+                abreFormularioAlunoActivity();
             }
         });
+    }
+
+    private void abreFormularioAlunoActivity() {
+        startActivity(new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        configuraListaAlunos();
+    }
 
-        AlunoDao alunoDao = new AlunoDao();
-
+    private void configuraListaAlunos() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunoDao.getAll());
         listaDeAlunos.setAdapter(adapter);
